91b05059b3037a8ef3768a1f083d3f4be9a0a921
Charina Vallecera <charinavallecera08@gmail.com>
2022-12-15 23:10:02 +0800

    5th commit

---
+++ graph.py
@@ -0,0 +1,14 @@
+# graph.py
+
+from typing import NamedTuple
+
+
+class City(NamedTuple):
+    name: str
+    country: str
+    year: int | None
+    latitude: float
+    longitude: float
+
+    @classmethod
+    def from_dict(cls, attrs):
