4318712425f995896bae8e71c2ba264cc110da48
Charina Vallecera <charinavallecera08@gmail.com>
2022-12-15 23:20:01 +0800

    6th commit - graph.py

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 # graph.py
 
 from typing import NamedTuple
+import networkx as nx
 
 
 class City(NamedTuple):
@@ -12,3 +13,21 @@ class City(NamedTuple):
 
     @classmethod
     def from_dict(cls, attrs):
+        return cls(
+            name=attrs["xlabel"],
+            country=attrs["country"],
+            year=int(attrs["year"]) or None,
+            latitude=float(attrs["latitude"]),
+            longitude=float(attrs["longitude"]),
+        )
+
+    def load_graph(filename, node_factory):
+        graph = nx.nx_agraph.read_dot(filename)
+        nodes = {
+            name: node_factory(attributes)
+            for name, attributes in graph.nodes(data=True)
+        }
+        return nodes, nx.Graph(
+            (nodes[name1], nodes[name2], weights)
+            for name1, name2, weights in graph.edges(data=True)
+        )
