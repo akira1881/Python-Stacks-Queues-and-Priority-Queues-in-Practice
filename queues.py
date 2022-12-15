daec5d07affd05040312ca185e00986d829a03e2
Charina Vallecera <charinavallecera08@gmail.com>
2022-12-15 22:23:43 +0800

    2nd commit

--- queues.py
+++ queues.py
@@ -1,6 +1,7 @@
 # queues.py
 
 from collections import deque
+from heapq import heappop, heappush
 
 
 class Queue:
@@ -24,3 +25,14 @@ class Queue:
 class Stack(Queue):
     def dequeue(self):
         return self._elements.pop()
+
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)[1]
