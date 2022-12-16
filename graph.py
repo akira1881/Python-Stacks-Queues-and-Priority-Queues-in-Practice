4b404a73064c3117966c47f02c5a5737f8d84d04
Charina Vallecera <charinavallecera08@gmail.com>
2022-12-16 16:47:21 +0800

    14 commit - graph edited

--- graph.py
+++ graph.py
@@ -2,8 +2,10 @@
 
 from typing import NamedTuple
 import networkx as nx
-from graph import City, load_graph
-from queues import Queue
+from queues import Queue, Stack
+from collections import deque
+from math import inf as infinity
+from queues import MutableMinHeap, Queue, Stack
 
 
 class City(NamedTuple):
@@ -37,9 +39,43 @@ class City(NamedTuple):
     def breadth_first_traverse(graph, source):
         queue = Queue(source)
         visited = {source}
-        for node in queue:
-            yield node
-            for neighbor in graph.neighbors(node):
+        while queue:
+            yield node := queue.dequeue()
+            neighbors = list(graph.neighbors(node))
+            if order_by:
+                neighbors.sort(key=order_by)
+            for neighbor in neighbor:
                 if neighbor not in visited:
                     visited.add(neighbor)
                     queue.enqueue(neighbor)
+
+    def breadth_first_search(graph, source, predicate, order_by=None):
+        return search(depth_first_traverse, graph, source, predicate, order_by)
+
+    def search(traverse, graph, source, predicate, order_by=None):
+    for node in traverse(graph, source, order_by):
+        if predicate(node):
+            return node
+
+    def dijkstra_shortest_path(graph, source, destination, weight_factory):
+        previous = {}
+        visited = set()
+
+        unvisited = MutableMinHeap()
+        for node in graph.nodes:
+            unvisited[node] = infinity
+        unvisited[source] = 0
+
+        while unvisited:
+            visited.add(node := unvisited.dequeue())
+            for neighbor, weights in graph[node].items():
+                if neighbor not in visited:
+                    weight = weight_factory(weights)
+                    new_distance = unvisited[node] + weight
+                    if new_distance < unvisited[neighbor]:
+                        unvisited[neighbor] = new_distance
+                        previous[neighbor] = node
+                    
+        return retrace(previous, source, destination)
+
+
